5e1da2ee8eb92bcd7ca7912ce7e1fd72b66852bf
Vijaisriraam <VijaiGopalS@dal.ca>
2019-01-30 10:46:33 -0400

    modified to add ipadress

--- myclass.py
+++ myclass.py
@@ -1,8 +1,32 @@
 class myMac():
 	"""this is a class creating and storing mac addresses"""
-	def __init__(self):
-		mlist=[0x00,0x00,0x00,0x00,0x00,0x00]
+	def __init__(self,maclist=[0x00,0x00,0x00,0x00,0x00,0x00]):
+		self.mlist=maclist
 	
 thispc=myMac()
-print(thispc.mlist)
 
+def printmac(somemac):
+	print(thispc.mlist)
+
+class cellphone():
+	def __init__(self,pname,pmodel):
+		self.name=pname
+		self.macadd=myMac([0xFF,0xFF,0xFF,0xFF,0xFF,0xFF])
+		self.model=pmodel
+
+class ipaddress():
+	def __init__(self,mname,mmodel):
+		self.name=mname
+		self.ipadd=myMac([0xFF,0xFF,0xFF,0xFF])
+		self.model=mmodel
+
+printmac(thispc)
+
+newphone=cellphone('raghu','MTA23C')
+print(newphone.name)
+
+myphone=ipaddress('vijai','IPXS')
+print(myphone.name)
+
+printmac(newphone.macadd)
+printmac(myphone.ipadd)
